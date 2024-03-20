26640939f4353535ec7f46a1d3f3644403988ef5
UnEvolvedGod <50989656+EdgarQuinones@users.noreply.github.com>
2024-03-20 03:43:40 -0400

    Update shred_files.sh

--- Scripts/shred_files.sh
+++ Scripts/shred_files.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Made by Bruce
+"""
+This script was designed by Bombenheimer(Bruce) as part of the NCAE competition.
+
+Follow me on GitHub for more projects like these and to collaborate!
+
+https://github.com/Bombenheimer/
+
+"""
 shred_malware() {
     local file
     find / -type f -name 'Malicious File' 2>/dev/null | while read -r file; do
